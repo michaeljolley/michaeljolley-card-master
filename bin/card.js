2a208ace64a9ed44a61a33390bc4823e208fd3bd
Michael Jolley <mike@sparcapp.io>
2019-06-07 17:17:53 -0500

    Updating card

--- bin/card.js
+++ bin/card.js
@@ -8,58 +8,19 @@ const boxen = require( "boxen" );
 const newline = "\n";
 
 const banner = `
-                                       '.-://:--..''                                                
-                                .:+ooooo+/:::://///++++//:'                                         
-                            ./+o/:......................-:+o+:                                      
-                          :o+-............................-::+yo-                                   
-                       '/o:.................................-::/os+.                                
-                      /o:....................................-::::/ss:'                             
-                    .o:........................................-:::::+y/                            
-                   /s-..........................................-::::::yy.                          
-                 'oo.............................................-::::::sd+                         
-                'so...............................................-::::::ody'                       
-               'so-...............................................-:::::::+hy                       
-               oo-.................................................:::::::/+d+                      
-              -y:..................................................::::::::+om-                     
-              y/-..................................................::::::::/+yy                     
-             :y:-.................................................-:::::::::+om.                    
-             y+:-..................................--::/+++ooso+:---::::::::++sy                    
-            'N::-..............................-:+sssyhhhhhhhysso/-.-:::::::+++y-.-.                
-            'N/::-.............................:yhyhhdhso/:-.........-::::::++++s::s/               
-             do:::........-----:://:-.........../o+/:-................-::::::/+so//.d'              
-             /d::::-.-:+syyhhddhdhhho.............---://oo+sso//-......-::::::syhs+-d.              
-              y/:::/oyhhhhyysssoooo/-...........--.:+o-:mmsdmy.-/-......-:::::+hhd//d.              
-              .+::/o+/:-...........--:/+-......:-.-://..//::-............-:::::dym+yho              
-               -::-........-/+osyo/o+--/y......:-....---------...........-::::-dhmo:hm'             
-               .::-.....-+++mNhhh/.--..-N-................................::::-dyms-od:             
-               .o::....:::-.:-..-:::-..-d.................................:::::dhhh::o:             
-               'h::..........--:-......:-.................................:::::dydm/:+'             
-               'm::....................-..........-...:+-................-:::::mydmo/oo             
-               -d+:...................o-..........-....:m:--............-:::::/myddh+ss             
-               -hh:...............-::.//.........:ooo-./h:...----.......::::+shhhhdm//              
-               .hN:.............--.h-.:++:....-+yyo+//+o+::::-...--...-/+oyhysyshhdm:               
-                dms-...........--..+h/oooyhhsyhyyhhhhhhhyhdddddhs:--.+yyyssssssyyddmo               
-                hhd::-.......---....:/+syhhhhyyhhyyyshshssyhhhdddm-./hsssssysyyhyhdmy               
-                sym+:::--...-:....:+shddhyyyhyyhsysyyysysssyssyhhm-.oyssssyyhsdydhddm.:             
-                +hyy::::::-.--./shddddyyysssysshssssyssyyhysyssshs:oysssysyyhyhdhdddN/:'            
-                .dyssoo++:-..-.mhdhhyhsssssssyyyyyyhddhys/--:sysyossssyshyshydydhdddN//.            
-                 dhssssyyyo-...mydyhyysyhhhhhhhyyso+/:-......+dyssssysyyydsyhddydhddN:+'            
-                 +dhshysssyy/..+hhyys+--::::-----:/o+-.....+yyysssysyhsdshhsdhdhhdddN:+'            
-                 'dhydyyssssh+../yyss+..---/syyhhdddyy+:/+shsssssyshsdyhhydhydddddddd:/             
-                  odhdydysyssh++osssyo-:-.:yyhhyhhhhddysssssssssshyhyhdydhhdyddddddmh:/             
-                  'dddhdyyhsysyssssssdyyh-+yhyhyyyyhyssssssssysssydydydhhdhddhdddddmo--'            
-                   +mdddyhhsdsyysysssssshyysssssssssssssssssyhhshsdhhhddhddddhddddmy'               
-                   'mdddhdhydshyyysssssysssssssssssssssyyyyyyhdhhhydhdhdddddddddddo'                
-                    +mddddhhdsdyhyysyyshyyysyssysssysyshhhyhyyddmdyddddddddddddmy-                  
-                    'ymdddhhdydydyysmyyyhyhshyyyyyyyyyhsdhyydsydNhdhddddddddddy:                    
-                     'smdddddhdhdyyhmhsydydydydydsdsdyhhydh/ydodmydhdddddddds-                      
-                       /dddddddddhydNh+hyhdhdyhydyhhhdhdsydy+dmdhddddddddy/'                        
-                        '+dmddddddhddmsdoyddyhohhdhhhddddsmdmyhdhdddddy/.                           
-                          '/ydmdddddydhmdhmmydyymmmdmmmddhshddhddddy/.                              
-                             ':ohmmmddyhyyhhhdhhmhhdyhdhddhddddyo:'                                 
-                                  -/oyhdmdddddhdddmmmdhyyso/:.                                      
-                                        ''...'''..''                                                
-
+███╗   ███╗██╗ ██████╗██╗  ██╗ █████╗ ███████╗██╗     
+████╗ ████║██║██╔════╝██║  ██║██╔══██╗██╔════╝██║     
+██╔████╔██║██║██║     ███████║███████║█████╗  ██║     
+██║╚██╔╝██║██║██║     ██╔══██║██╔══██║██╔══╝  ██║     
+██║ ╚═╝ ██║██║╚██████╗██║  ██║██║  ██║███████╗███████╗
+╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+                                                      
+      ██╗ ██████╗ ██╗     ██╗     ███████╗██╗   ██╗    
+      ██║██╔═══██╗██║     ██║     ██╔════╝╚██╗ ██╔╝    
+      ██║██║   ██║██║     ██║     █████╗   ╚████╔╝     
+ ██   ██║██║   ██║██║     ██║     ██╔══╝    ╚██╔╝      
+ ╚█████╔╝╚██████╔╝███████╗███████╗███████╗   ██║       
+  ╚════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝       
 `;
 
 // Define options for Boxen
@@ -84,22 +45,21 @@ const data = {
 	labelTwitter: chalk.hex("#1DA1F2").bold( " Twitter:" ),
 	labelGitHub: chalk.hex("#D7DCE3").bold( "  GitHub:" ),
 	labelTwitch: chalk.hex("#8A6DC1").bold( "Twitch:" ),
-	labelWeb: chalk.white.bold( "Web:" ),
+	labelWeb: chalk.white.bold( "Blog:" ),
 	labelCard: chalk.white.bold( "    Card:" )
 };
 
 // Actual strings we're going to output
-const heading = `                                           ${ data.name }`;
-const working = `\n                                      ${ data.work }\n`;
-const twittering = `${ data.labelTwitter }  ${ data.twitter }`;
-const githubing = `${ data.labelGitHub }  ${ data.github }`;
-const twitching = `${ data.labelTwitch }  ${ data.twitch }`;
-const webing = `${ data.labelWeb }  ${ data.web }`;
-const carding = `\n${ data.labelCard }  ${ data.npx }`;
+const working = `               ${ data.work }\n`;
+const twittering = `    ${ data.labelTwitter }  ${ data.twitter }`;
+const githubing = `    ${ data.labelGitHub }  ${ data.github }`;
+const twitching = `      ${ data.labelTwitch }  ${ data.twitch }`;
+const webing = `        ${ data.labelWeb }  ${ data.web }`;
+const carding = `\n    ${ data.labelCard }  ${ data.npx }`;
 
-const set1 = [data.banner, heading, working];
-const set2 = twittering + `           ` + twitching;
-const set3 = githubing + `               ` + webing;
+const set1 = [data.banner, working];
+const set2 = twittering + `\n` + githubing;
+const set3 = twitching + `\n` + webing;
 
 const card = [
 	set1.join(newline), set2, set3, carding
