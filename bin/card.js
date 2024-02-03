c7fcc7b27f95fb5edad5dcc1b93d576dea48c758
Michael Jolley <mike@baldbeardedbuilder.com>
2024-02-03 16:53:01 -0600

    Updating info

--- bin/card.js
+++ bin/card.js
@@ -2,47 +2,28 @@
 // 👆 Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
-const chalk = require("chalk");
-const boxen = require("boxen");
+import chalk from "chalk";
+import boxen from "boxen";
+import gradient from "gradient-string";
 
 const newline = "\n";
-
-const banner = `
-  ███╗   ███╗██╗ ██████╗██╗  ██╗ █████╗ ███████╗██╗     
-  ████╗ ████║██║██╔════╝██║  ██║██╔══██╗██╔════╝██║     
-  ██╔████╔██║██║██║     ███████║███████║█████╗  ██║     
-  ██║╚██╔╝██║██║██║     ██╔══██║██╔══██║██╔══╝  ██║     
-  ██║ ╚═╝ ██║██║╚██████╗██║  ██║██║  ██║███████╗███████╗
-  ╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
-                                                        
-       ██╗ ██████╗ ██╗     ██╗     ███████╗██╗   ██╗    
-       ██║██╔═══██╗██║     ██║     ██╔════╝╚██╗ ██╔╝    
-       ██║██║   ██║██║     ██║     █████╗   ╚████╔╝     
-  ██   ██║██║   ██║██║     ██║     ██╔══╝    ╚██╔╝      
-  ╚█████╔╝╚██████╔╝███████╗███████╗███████╗   ██║       
-   ╚════╝  ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝       
-                                                        
-`;
 
 // Define options for Boxen
 const options = {
   padding: 1,
   margin: 1,
-  borderColor: "#366880"
+  borderColor: "#366880",
+  borderStyle: "round"
 };
 
 // Text + chalk definitions
 const data = {
-  banner: chalk.hex("#5C7480")(banner), //#20B968
-  name: chalk.white("Michael Jolley"),
+  name: chalk.white.bold("Michael Jolley"),
   handle: chalk.cyan("michaeljolley"),
-  work:
-    chalk.hex("#947E39")("Bald. ") +
-    chalk.hex("#B89C46")("Bearded. ") +
-    chalk.hex("#FAD667")("Builder."),
-  twitter: chalk.white("https://twitter.com/baldbeardbuild"),
+  work: gradient("#5DE4C7", "#236092")("bald bearded builder"),
+  twitter: chalk.white("https://twitter.com/michaeljolley"),
   github: chalk.white("https://github.com/michaeljolley"),
-  twitch: chalk.white("https://twitch.tv/baldbeardedbuilder"),
+  twitch:  chalk.white("https://twitch.tv/baldbeardedbuilder"),
   youtube: chalk.white("https://youtube.com/c/baldbeardedbuilder"),
   web: chalk.white("https://baldbeardedbuilder.com"),
   npx: chalk.white("npx michaeljolley"),
@@ -50,12 +31,12 @@ const data = {
   labelGitHub: chalk.hex("#666666").bold("  GitHub:"),
   labelTwitch: chalk.hex("#8A6DC1").bold("Twitch:"),
   labelYouTube: chalk.hex("#FF0000").bold("YouTube:"),
-  labelWeb: chalk.white.bold("Blog:"),
+  labelWeb: chalk.hex("#5DE4C7").bold("Blog:"),
   labelCard: chalk.white.bold("    Card:")
 };
 
 // Actual strings we're going to output
-const working = `               ${data.work}\n`;
+const working = `                    ${data.name}\n                 ${data.work}\n`;
 const twittering = `    ${data.labelTwitter}  ${data.twitter}`;
 const githubing = `    ${data.labelGitHub}  ${data.github}`;
 const youtubing = `     ${data.labelYouTube}  ${data.youtube}`;
@@ -63,9 +44,9 @@ const twitching = `      ${data.labelTwitch}  ${data.twitch}`;
 const webing = `        ${data.labelWeb}  ${data.web}`;
 const carding = `\n    ${data.labelCard}  ${data.npx}`;
 
-const set1 = [data.banner, working];
-const set2 = twittering + `\n` + githubing;
-const set3 = youtubing + `\n` + twitching + `\n` + webing;
+const set1 = [working];
+const set2 = twittering + `\n` + youtubing;
+const set3 = twitching + `\n` + githubing + `\n` + webing;
 
 const card = [set1.join(newline), set2, set3, carding];
 
