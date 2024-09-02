5a6f02582ff6b8f69b7f941594a6ef588e9facd5
Michael Jolley <mike@baldbeardedbuilder.com>
2024-09-02 13:47:22 -0500

    Adding LinkedIn and realigning

--- bin/card.js
+++ bin/card.js
@@ -21,36 +21,34 @@ const data = {
   name: chalk.white.bold("Michael Jolley"),
   handle: chalk.cyan("michaeljolley"),
   work: gradient("#5DE4C7", "#236092")("bald bearded builder"),
-  twitter: chalk.white("https://twitter.com/michaeljolley"),
+  twitter: chalk.white("https://x.com/michaeljolley"),
   github: chalk.white("https://github.com/michaeljolley"),
   twitch:  chalk.white("https://twitch.tv/baldbeardedbuilder"),
   youtube: chalk.white("https://youtube.com/c/baldbeardedbuilder"),
   web: chalk.white("https://baldbeardedbuilder.com"),
+  linkedin: chalk.white("https://linkedin.com/in/michaelwjolley"),
   npx: chalk.white("npx michaeljolley"),
-  labelTwitter: chalk.hex("#1DA1F2").bold(" Twitter:"),
-  labelGitHub: chalk.hex("#666666").bold("  GitHub:"),
+  labelTwitter: chalk.hex("#1DA1F2").bold("Twitter (X):"),
+  labelGitHub: chalk.hex("#666666").bold("GitHub:"),
   labelTwitch: chalk.hex("#8A6DC1").bold("Twitch:"),
   labelYouTube: chalk.hex("#FF0000").bold("YouTube:"),
+  labelLinkedIn: chalk.hex("#0A66C2").bold("LinkedIn:"),
   labelWeb: chalk.hex("#5DE4C7").bold("Blog:"),
-  labelCard: chalk.white.bold("    Card:")
+  labelCard: chalk.white.bold("Card:")
 };
 
 // Actual strings we're going to output
-const working = `                    ${data.name}\n                 ${data.work}\n`;
-const twittering = `    ${data.labelTwitter}  ${data.twitter}`;
-const githubing = `    ${data.labelGitHub}  ${data.github}`;
-const youtubing = `     ${data.labelYouTube}  ${data.youtube}`;
-const twitching = `      ${data.labelTwitch}  ${data.twitch}`;
-const webing = `        ${data.labelWeb}  ${data.web}`;
-const carding = `\n    ${data.labelCard}  ${data.npx}`;
-
-const set1 = [working];
-const set2 = twittering + `\n` + youtubing;
-const set3 = twitching + `\n` + githubing + `\n` + webing;
-
-const card = [set1.join(newline), set2, set3, carding];
+const lines = [];
+lines.push(`                    ${data.name}\n                 ${data.work}\n`);
+lines.push(`     ${data.labelYouTube}  ${data.youtube}`);
+lines.push(` ${data.labelTwitter}  ${data.twitter}`);
+lines.push(`      ${data.labelTwitch}  ${data.twitch}`);
+lines.push(`      ${data.labelGitHub}  ${data.github}`);
+lines.push(`    ${data.labelLinkedIn}  ${data.linkedin}`);
+lines.push(`        ${data.labelWeb}  ${data.web}`);
+lines.push(`\n        ${data.labelCard}  ${data.npx}`);
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = card.join(newline);
+const output = lines.join(newline);
 
 console.log(chalk.green(boxen(output, options))); // eslint-disable-line no-console
